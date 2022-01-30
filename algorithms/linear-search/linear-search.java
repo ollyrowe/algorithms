c3c3078f4331f818a4611bcd0dd03a0d6b53b9f7
emilielake <emilielake2002@gmail.com>
2022-01-30 18:35:58 +0000

    feat(algorithms): add linear search examples

---
+++ algorithms/linear-search/linear-search.java
@@ -0,0 +1,9 @@
+class LinearSearch {
+	public static int linearSearch(int array[], int value) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value)
+				return i;
+		}
+		return -1;
+	}
+}
